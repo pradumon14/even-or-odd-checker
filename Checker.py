6364cb658766933d8c6038b241cd7360c422112f
Pradumon <88714734+Pradumon12@users.noreply.github.com>
2022-12-12 16:12:56 +0530

    Create Checker.py

---
+++ Checker.py
@@ -0,0 +1,13 @@
+# Take input from the user
+
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+
+if num % 2 == 0:
+
+  print(num, "is even.")
+
+else:
+
+  print(num, "is odd.")
